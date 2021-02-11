6dfb3fd7fb51b1414ef9b0007d01a7ad43d9491d
bichiko <b.kodua@gmail.com>
2021-02-12 00:10:06 +0200

    docker-compose support

--- server/src/database/index.ts
+++ server/src/database/index.ts
@@ -2,16 +2,23 @@ import mongoose from 'mongoose';
 import log from '../logger';
 
 const initDatabase = async (): Promise<void> => {
-    const {MONGO_URL, MONGO_DB_NAME} = process.env;
+    const {
+        MONGO_DB_NAME,
+        MONGO_HOST,
+        MONGO_PORT,
+        MONGO_USER,
+        MONGO_PASS
+    } = process.env;
+    const url = `mongodb://${MONGO_USER}:${MONGO_PASS}@${MONGO_HOST}:${MONGO_PORT}/${MONGO_DB_NAME}`;
     const mongoOptions = {
         useNewUrlParser: true,
         useUnifiedTopology: true,
         useFindAndModify: false,
         useCreateIndex: true
     };
-    await mongoose.connect(`${MONGO_URL}/${MONGO_DB_NAME}`, mongoOptions)
+    await mongoose.connect(url, mongoOptions)
         .catch((error) => {
-            log.info(`Connection string is "${MONGO_URL}/${MONGO_DB_NAME}"`);
+            log.info(`Connection string is "${url}"`);
             throw new Error(error.message || error);
         });
     log.info('Database connection established successfully');
